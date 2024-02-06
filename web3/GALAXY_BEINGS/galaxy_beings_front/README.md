9b9da3ba0d7da3f936aead8bdfd62bb402ee3ba3
Jay zhu <44246625+jiliangzhu@users.noreply.github.com>
2024-02-06 16:12:03 +0800

    Remove folder-name

--- web3/GALAXY_BEINGS/galaxy_beings_front/README.md
+++
@@ -1,1 +0,0 @@
-# galaxy_beings_front
--- web3/GALAXY_BEINGS/galaxy_beings_front/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './static/Index.css'
-import  App  from './App';
-require('events').EventEmitter.prototype._maxListeners = 100;
-ReactDOM.render(<App />, document.getElementById('root'));
