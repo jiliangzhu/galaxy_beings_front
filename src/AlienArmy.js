11a077a6d8e72388b1f9722f459db8e0f149de30
Jay zhu <44246625+jiliangzhu@users.noreply.github.com>
2024-02-12 11:31:48 +0800

    modify alienarmy.js

--- src/AlienArmy.js
+++ src/AlienArmy.js
@@ -2,107 +2,109 @@ import React, { Component } from 'react'
 import AlienCard from "./AlienCard";
 import './static/AlienPreview.css';
 import MyWeb3 from './MyWeb3'
-import {
-    BrowserRouter as 
-    // eslint-disable-next-line no-unused-vars
-    Route,
-    Link
-  } from "react-router-dom"
+import { BrowserRouter as Route, Link } from "react-router-dom"
 // eslint-disable-next-line no-unused-vars
- import Page from "./Page";
+import Page from "./Page";
 
 class AlienArmy extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             aliens: [],
+            totalAliens: 0,  // 总外星人数
             currentPage: 1,
-            aliensPerPage: 30 // 每页显示30个僵尸
+            aliensPerPage: 30 // 每页显示30个外星人
         };
     }
-        
+    
     componentDidMount(){
-        let that = this
-        let ethereum = window.ethereum
+        this.initialize();
+    }
+
+    initialize = () => {
+        let that = this;
+        let ethereum = window.ethereum;
         if (typeof ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
-            MyWeb3.init().then(function(res){
-                that.alienArmy()
-            })
-        }else {
-            alert('You have to install MetaMask !')
+            MyWeb3.init().then(function(){
+                that.loadTotalAlienCount();
+            });
+        } else {
+            alert('You have to install MetaMask !');
         }
     }
-    alienArmy(){
-        let that = this;
-        MyWeb3.alienCount().then(function(result){
-            console.log("Total aliens:", result); // 打印总僵尸数
-            let promises = [];
-            for(let i = 0; i < result; i++){
-                promises.push(MyWeb3.aliens(i));
-            }
-        
-            Promise.all(promises).then(results => {
-                console.log("All aliens data:", results); // 打印所有僵尸数据
-                const aliensWithId = results.map((alien, index) => {
-                    return {...alien, alienId: index};
-                });
-                that.setState({ aliens: aliensWithId });
+
+    loadTotalAlienCount = () => {
+        MyWeb3.alienCount().then(totalCount => {
+            this.setState({ totalAliens: totalCount });
+            this.loadAliens();
+        });
+    }
+
+    loadAliens = () => {
+        const { currentPage, aliensPerPage, totalAliens } = this.state;
+        const startIndex = (currentPage - 1) * aliensPerPage;
+        const endIndex = Math.min(startIndex + aliensPerPage, totalAliens);
+
+        let promises = [];
+        for (let i = startIndex; i < endIndex; i++) {
+            promises.push(MyWeb3.aliens(i));
+        }
+
+        Promise.all(promises).then(results => {
+            const aliensWithId = results.map((alien, index) => {
+                return {...alien, alienId: startIndex + index};
             });
+            this.setState({ aliens: aliensWithId });
         });
     }
 
     componentWillUnmount() {
         this.setState = (state, callback) => {
-            return
+            return;
         }
     }
 
-handleClick = (event) => {
-    const newPage = Number(event.target.id);
-    console.log("Current page:", newPage); // 打印当前点击的页码
-    this.setState({
-        currentPage: newPage
-    });
-}
-
-
-
-render() {
-    const { aliens, currentPage, aliensPerPage } = this.state;
-    const indexOfLastAlien = currentPage * aliensPerPage;
-    const indexOfFirstAlien = indexOfLastAlien - aliensPerPage;
-    const currentAliens = aliens.slice(indexOfFirstAlien, indexOfLastAlien);
-    console.log("Aliens on current page:", currentAliens); 
-    const renderAliens = currentAliens.map((alien, index) => {
-        return (
-             <Link to={`?AlienDetail&id=${alien.alienId}`} key={alien.alienId}>
-                <AlienCard alien={alien} name={alien.name} level={alien.level}></AlienCard>
-            </Link>
-        );
-    });
-
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(aliens.length / aliensPerPage); i++) {
-        pageNumbers.push(i);
+    handleClick = (event) => {
+        const newPage = Number(event.target.id);
+        this.setState({
+            currentPage: newPage
+        }, () => {
+            this.loadAliens();  // 在设置新页面后重新加载外星人
+        });
     }
 
-    const renderPageNumbers = pageNumbers.map(number => {
-        return (
-            <li key={number} id={number} onClick={this.handleClick}>
-                {number}
-            </li>
-        );
-    });
+    render() {
+        const { aliens } = this.state;
+        const renderAliens = aliens.map(alien => {
+            return (
+                <Link to={`?AlienDetail&id=${alien.alienId}`} key={alien.alienId}>
+                    <AlienCard alien={alien} name={alien.name} level={alien.level}></AlienCard>
+                </Link>
+            );
+        });
 
-    return (
-        <div className="alien-army-container">
-            <div className="cards">{renderAliens}</div>
-            <div className="pagination">
-                <ul className="page-numbers">{renderPageNumbers}</ul>
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(this.state.totalAliens / this.state.aliensPerPage); i++) {
+            pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+                <li key={number} id={number} onClick={this.handleClick}>
+                    {number}
+                </li>
+            );
+        });
+
+        return (
+            <div className="alien-army-container">
+                <div className="cards">{renderAliens}</div>
+                <div className="pagination">
+                    <ul className="page-numbers">{renderPageNumbers}</ul>
+                </div>
             </div>
-        </div>
         );
     }
 }
- 
+
 export default AlienArmy;
